ba20264393c40c748d9570b2a5eafcf41d0bf832
Siber92 <44703126+Siber92@users.noreply.github.com>
2020-01-28 08:57:10 +0100

    Update colleagues_sample.sql

--- netbeans/colleagues_sample.sql
+++ netbeans/colleagues_sample.sql
@@ -1,13 +1,9 @@
-CREATE TABLE COLLEAGUES (
+CREATE TABLE FRIENDS (
    "ID" INTEGER not null primary key,
-    "FIRSTNAME" VARCHAR(30),
-    "LASTNAME" VARCHAR(30),
-    "TITLE" VARCHAR(10),
-    "DEPARTMENT" VARCHAR(20),
-    "EMAIL" VARCHAR(60)
+    "NAME" VARCHAR(256)
 );
 
-INSERT INTO COLLEAGUES VALUES (1,'Mike','Johnson','Manager','Engineering','mike.johnson@foo.com');
-INSERT INTO COLLEAGUES VALUES (2, 'James', 'Still', 'Engineer', 'Engineering', 'james.still@foo.com');
-INSERT INTO COLLEAGUES VALUES (3, 'Jerilyn', 'Stall', 'Manager', 'Marketing', 'jerilyn.stall@foo.com');
-INSERT INTO COLLEAGUES VALUES (4, 'Jonathan', 'Smith', 'Manager', 'Marketing', 'jonathan.smith@foo.com');
+INSERT INTO COLLEAGUES VALUES (1,'Mike);
+INSERT INTO COLLEAGUES VALUES (2, 'James');
+INSERT INTO COLLEAGUES VALUES (3, 'Jerilyn');
+INSERT INTO COLLEAGUES VALUES (4, 'Jonathan');
